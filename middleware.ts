25bf886918f39de7c8139f20df923c29877e5cd2
Sergey Primakovich <prima.psa@gmail.com>
2024-05-26 15:15:07 +0300

    feat: add vercel4

--- middleware.ts
+++ middleware.ts
@@ -5,15 +5,15 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest) {
     const requestHeaders = new Headers(req.headers);
 
+    // Добавление заголовка ко всем запросам
     requestHeaders.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMThlZjVjOTFjNDkzNDA5NGY2ZTk3YzUzNDEwYjQ1MyIsInN1YiI6IjY2M2Y5NmVjMTMyNzIxZjUxODIxMGJjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MHRIcWru0tXRfowkGqX1dJnfJoTCMAnKn3WDWY5ilYQ');
 
-    const response = NextResponse.next({
+
+    return NextResponse.rewrite(new URL(req.url, req.nextUrl.origin), {
         request: {
             headers: requestHeaders,
         },
     });
-
-    return response;
 }
 
 export const config = {
