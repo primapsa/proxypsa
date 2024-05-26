f619389c3ee42903b9438fee60acaa11e6d1de0a
Sergey Primakovich <prima.psa@gmail.com>
2024-05-26 16:11:08 +0300

    feat: add vercel9

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -4,13 +4,22 @@ import httpProxy from 'http-proxy';
 const proxy = httpProxy.createProxyServer();
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Проверка на наличие req.url
+    if (!req.url) {
+        res.status(500).json({ error: 'Request URL is missing' });
+        return;
+    }
+
+    // Извлечение пути из запроса и построение нового URL для целевого сервера
+    const targetUrl = `https://api.themoviedb.org${req.url.replace('/api/proxy', '')}`;
+
     // Добавление заголовков ко всем запросам
     req.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMThlZjVjOTFjNDkzNDA5NGY2ZTk3YzUzNDEwYjQ1MyIsInN1YiI6IjY2M2Y5NmVjMTMyNzIxZjUxODIxMGJjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MHRIcWru0tXRfowkGqX1dJnfJoTCMAnKn3WDWY5ilYQ';
-    req.headers['X-Custom-Header'] = 'your-custom-header-value';
-
+    req.headers['X-Custom-Header'] = `${targetUrl}`;
+    console.log(`Proxying request to: ${targetUrl}`);
     // Перенаправление на целевой сервер
     proxy.web(req, res, {
-        target: 'https://api.themoviedb.org',
+        target: targetUrl,
         changeOrigin: true,
         selfHandleResponse: false,
     });
